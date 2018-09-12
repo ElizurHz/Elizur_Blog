7b37ad34c2f720274318fe662886bb3620a43594
Haoze Xu <elizurhz@gmail.com>
2018-09-12 16:28:34 +0800

    blog-init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  dest: 'Elizur_Blog',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'Elizur_Blog',
+      description: '一个不善表达的闷骚前端的个人小空间'
+    }
+  },
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  serviceWorker: true,
+  themeConfig: {
+    editLinks: true,
+    docsDir: 'docs',
+    locales: {
+      '/': {
+        label: '简体中文',
+        selectText: '选择语言',
+        editLinkText: '编辑此页',
+        lastUpdated: '上次更新',
+        nav: [
+          {
+            text: '微信小程序',
+            link: '/wx/mpvue-wx-mini-app-first-look'
+          }
+        ],
+        sidebar: {
+          '/wx/': genSidebarConfig('微信小程序')
+        }
+      }
+    }
+  }
+}
+
+function genSidebarConfig (title) {
+  return [
+    {
+      title,
+      collapsable: false,
+      children: [
+        'mpvue-wx-mini-app-first-look',
+      ]
+    }
+  ]
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+# heroImage: /hero.png
+actionText: 关于我 →
+actionLink: /about-me/
+footer: elizurhz@gmail.com | 转载需获得本人许可 | Copyright © 2018-present Elizur_
+---
